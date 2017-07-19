b589f58b06dcd8aa0da9a74f43cb75408f76e09d
miguel-rodriguez <miguel220369@gmail.com>
2017-07-19 11:14:21 +0100

    Updating README.md file

--- src/main/java/com/alfresco/support/alfrescodb/dao/SolrMemoryMapper.java
+++ src/main/java/com/alfresco/support/alfrescodb/dao/SolrMemoryMapper.java
@@ -10,10 +10,10 @@ import java.util.List;
 @Mapper
 public interface SolrMemoryMapper {
     @Select("select * from\n" +
-            "(select count( * ) alfrescoNodes from alf_node where store_id = (select id from alf_store where protocol = 'workspace' and identifier = 'SpacesStore')) as N1 ,\n" +
-            "(select count( * ) archiveNodes from alf_node where store_id = (select id from alf_store where protocol = 'archive' and identifier = 'SpacesStore')) as N2 ,\n" +
-            "(select count( * ) transactions from alf_transaction ) as T,\n" +
-            "(select count( * ) acls from alf_access_control_list ) as A,\n" +
-            "(select count( * ) aclTransactions from alf_acl_change_set) as X")
+            "(select count( * ) alfrescoNodes from alf_node where store_id = (select id from alf_store where protocol = 'workspace' and identifier = 'SpacesStore')) N1 ,\n" +
+            "(select count( * ) archiveNodes from alf_node where store_id = (select id from alf_store where protocol = 'archive' and identifier = 'SpacesStore')) N2 ,\n" +
+            "(select count( * ) transactions from alf_transaction ) T,\n" +
+            "(select count( * ) acls from alf_access_control_list ) A,\n" +
+            "(select count( * ) aclTransactions from alf_acl_change_set) X")
     List<SolrMemory> solrMemory();
 }
